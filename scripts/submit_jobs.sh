4c1d77b07b3dbbc0f34b516290ba0055347efaef
Peter Vrancx <pvrancx@vub.ac.be>
2017-02-03 15:20:19 +0100

    slurm script

--- scripts/submit_jobs.sh
+++ scripts/submit_jobs.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #
+#SBATCH -p short
 #SBATCH --job-name=intra_option
 #SBATCH --output=$HOME/Code/fourier/log.txt
 #
-#SBATCH --ntasks=1
+#SBATCH sbatch --array=0-1
 #SBATCH --time=60:00
 
 cd $HOME/Code/fourier/fourier
-python run_exp.py -id $SLURM_PROCID
+python run_exp.py -id $SLURM_ARRAY_TASK_ID
